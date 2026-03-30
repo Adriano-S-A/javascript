b4d996048bc30df37df7d93cf86c8c6e88f4664f
Adriano S Alves <Adrs0000000001@gmail.com>
2026-03-30 14:33:54 -0300

    Aula 12

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 21;
+if(idade >= 16 && idade < 18 || idade > 65){
+    console.log('Voto é Opcional!')
+}else if(idade >= 18){
+    console.log('Voto Obrigatório!')
+}else{
+    console.log('Não Vota!!')
+}
