3fc55dcac10b4d6c0abdc67f2f500bd63e37e64b
Adriano S Alves <Adrs0000000001@gmail.com>
2026-05-30 17:45:35 -0300

    ex014 incompleto

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,17 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+   // var hora = data.getHours()
+   hora = 22
     msg.innerHTML = `Agora são ${hora} Horas.`
+    if (hora >= 0 && hora < 12){
+        //Boa Dia
+        img.src = 'manha.png'
+    }else if(hora >= 12 && hora < 18){
+        //Boa Tarde
+        img.src = 'tarde.png'
+    }else{
+        //Boa Noite
+        img.src = 'noite.png'
+    }
 }
