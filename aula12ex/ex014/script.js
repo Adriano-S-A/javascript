144e09bfd1e8ae5a318b5b603d9634ea2e12b48e
Adriano S Alves <Adrs0000000001@gmail.com>
2026-05-31 09:50:26 -0300

    ex014 completo

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,17 +1,21 @@
 function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} Horas.`
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+
+    msg.innerHTML = `Agora são ${hora} Horas.`;
     if (hora >= 0 && hora < 12){
         //Boa Dia
-        img.src = 'imagens/manha.png'
+        img.src = 'imagens/manha.png';
+        document.body.style.background = '#fdefa8';
     }else if(hora >= 12 && hora < 18){
         //Boa Tarde
-        img.src = 'imagens/tarde.png'
+        img.src = 'imagens/tarde.png';
+        document.body.style.background = '#e57e53';
     }else{
         //Boa Noite
-        img.src = 'imagens/noite.png'
+        img.src = 'imagens/noite.png';
+        document.body.style.background ='#2a4151';
     }
 }
